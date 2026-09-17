2fbf976e9bb30a58c81263fa4f7b24a6936a3ab7
luizfbalves <luizzbanndera@gmail.com>
2026-09-17 18:16:03 -0300

    initial commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../generated/prisma/client.js';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not set');
+    }
+
+    super({ adapter: new PrismaMariaDb(connectionString) });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
